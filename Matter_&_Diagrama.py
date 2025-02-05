7fcc8cb85c40708dae64fe214088c1102c6b6bc0
gibranacademy <gibranacademy7@gmail.com>
2025-02-05 18:08:15 +0200

    start

---
+++ Matter_&_Diagrama.py
@@ -0,0 +1,19 @@
+# To check if a number between 10 and 100 is prime, perform the following checks:
+#
+# Check if the number is divisible by 2 without a remainder. If yes, print "The number is not prime." Otherwise, continue.
+# Check if the number is divisible by 3 without a remainder. If yes, print "The number is not prime." Otherwise, continue.
+# Check if the number is divisible by 5 without a remainder. If yes, print "The number is not prime." Otherwise, continue.
+# Check if the number is divisible by 7 without a remainder. If yes, print "The number is not prime." Otherwise, print "The number is prime."
+# Test the above logic for the numbers: 12, 17, 23, 100, 121.
+#
+# Additionally, add a check to verify whether the number is within the range of 10 to 100.
+#
+# If the number is within the range, proceed to the next step.
+# Otherwise, print "The number is out of range" and exit.
+# Place this range check before the division check by 2 in the sequence.
+#
+# Finally, test the program for the numbers: 121, 169.
+
+###################################################################################
+
+1. Choosing the Right Design Pattern: Chain of Responsibility
